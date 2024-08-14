2e3658753e5c8c18e176c27c817dcbc53debcd8c
Wilian Zembrani <Wilian3355@gmail.com>
2024-08-13 21:28:40 -0300

    Criando o model Page

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -16,3 +16,34 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugfy_new(self.name)
         return super().save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(
+        unique=True, default=None,
+        null= True, blank=True, max_length=255,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(
+        unique=True, default='',
+        null=False, blank=True, max_length=255
+    )
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
