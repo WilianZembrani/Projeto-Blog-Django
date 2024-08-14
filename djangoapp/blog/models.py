e529c8fb2b4ede1e030708b1cb6031c5279f27cb
Wilian Zembrani <Wilian3355@gmail.com>
2024-08-14 20:37:31 -0300

    Criando o model Post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.rands import slugfy_new
+from django.contrib.auth.models import User
 
 class Tag(models.Model):
     class Meta:
@@ -42,8 +43,62 @@ class Page(models.Model):
         unique=True, default='',
         null=False, blank=True, max_length=255
     )
-    is_published = models.BooleanField(default=False)
+    is_published = models.BooleanField(
+    default=False,
+    help_text='Este campo precisará estar marcado para a página ser exibida plublicamente'
+    )
     content = models.TextField()
 
     def __str__(self) -> str:
         return self.title
+    
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=60)
+    slug = models.SlugField(
+        unique=True, default='',
+        null=False, blank=True, max_length=255
+    )
+
+    is_published = models.BooleanField(
+    default=False,
+    help_text='Este campo precisará estar marcado para a página ser exibida plublicamente'
+    )
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='Exibir a imagem de capa também dentro do conteúdo do post?',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, 
+        blank=True, null=True, 
+        related_name='post_created_by'
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='post_update_by'
+    )
+    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank= True,
+        default=None,
+    )
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+    
+    def __str__(self) -> str:
+        return self.title
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugfy_new(self.title)
+        return super().save(*args, **kwargs)
+    
+
