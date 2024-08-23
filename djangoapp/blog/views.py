cc09c985c83b9465b2e792b2e33c2615eb6a0197
Wilian Zembrani <Wilian3355@gmail.com>
2024-08-22 21:27:52 -0300

    Fazendo a Migração da view category e created_by para Class based view

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,48 +1,28 @@
+from typing import Any
 from django.core.paginator import Paginator
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from blog.models import Post, Page
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.http import Http404
+from django.http import Http404, HttpRequest, HttpResponse
 from django.views.generic import ListView
+from django.shortcuts import redirect, render
 
-PER_PAGE = 1
+PER_PAGE = 9
 
 class PostListView(ListView):
-    model = Post
     template_name =  'blog/pages/index.html'
     context_object_name = 'posts'
-    ordering = '-pk',
     paginate_by = PER_PAGE
     queryset = Post.objects.get_published()  #type:ignore
-
-    # def get_queryset(self):
-    #     queryset = super().get_queryset()
-    #     queryset = queryset.filter(is_published=True)
-    #     return queryset
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
         context.update({'page_title': 'Home - ',})
 
         return context
-
-# def index(request):
-#     posts = Post.objects.get_published() #type: ignore
-
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': 'Home -',
-#         }
-#     )
 
 
 def page(request, slug):
@@ -91,33 +71,59 @@ def post(request, slug):
         }
 
     )
+class CreatedByListView(PostListView):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._temp_context: dict[str, Any] = {}
 
-def created_by(request, author_pk):
-    user = User.objects.filter(pk=author_pk).first()
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
+        user_full_name = user.username
 
-    if user is None:
-        raise Http404()
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
+        page_title = 'Posts de ' + user_full_name + ' - ' 
 
-    posts = Post.objects.get_published() .filter(created_by__pk=author_pk)  #type:ignore
-    user_full_name = user.username
-
-    if user.first_name:
-        user_full_name = f'{user.first_name} {user.last_name}'
-    page_title = 'Posts de ' + user_full_name + ' - ' 
-
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
+        ctx.update({
+            'page_title':page_title,
+        })
+        return ctx
     
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-            'page_title': page_title ,
-        }
-    )
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return qs
+    
+    def get(self, request, *args, **kwargs,):
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk).first()
+
+        if user is None:
+            raise Http404
+        
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+        
+        return super().get(request, *args, **kwargs)
+
+class CategoryListView(PostListView):
+    allow_empty = False
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            category__slug= self.kwargs.get('slug')
+        )
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        page_title = f'{self.object_list[0].category.name} - Categoria - ' #type:ignore
+        ctx.update({
+            'page_title':page_title,
+        })
+        return ctx
 
 
 def category(request, slug):
